ab58564e81693f0a046833be3ab05f6fd80b47ac
Jumperplayercoo <benjaminzhimingzhang@gmail.com>
2024-08-16 10:33:23 -0700

    Changed titles and socials

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
+  title: 'Benjamin Zhang',
   subtitle: 'Demo Site',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
@@ -48,25 +48,30 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
+  name: 'Benjamin Zhang',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
-    {
+    /*{
       name: 'Twitter',
       icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
       url: 'https://twitter.com',
-    },
+    },*/
     {
+      name: 'Discord',
+      icon: 'fa6-brands:discord',
+      url: 'https://discord.com/users/738821494871687199',
+    },
+    /*{
       name: 'Steam',
       icon: 'fa6-brands:steam',
       url: 'https://store.steampowered.com',
-    },
+    },*/
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/Jumperplayercoo',
     },
   ],
 }
