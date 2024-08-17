156948cc6d28bb5e4b5c58969604d489145d8952
Jumperplayercoo <benjaminzhimingzhang@gmail.com>
2024-08-16 23:29:46 -0700

    Added banner and add more to the front page

--- src/config.ts
+++ src/config.ts
@@ -8,19 +8,19 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'Benjamin Zhang',
-  subtitle: 'Demo Site',
+  subtitle: 'AdBENtures in Life',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    enable: true,
+    src: 'assets/images/Thunderfin-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
     credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
+      enable: true,         // Display the credit text of the banner image
+      text: 'Ceira Motoyama',              // Credit text to be displayed
       url: ''                // (Optional) URL link to the original artwork or artist's page
     }
   },
@@ -38,18 +38,18 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Home,
     LinkPreset.Archive,
     LinkPreset.About,
-    {
+    /*{
       name: 'GitHub',
       url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
-    },
+    },*/
   ],
 }
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Benjamin Zhang',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  bio: 'A whimsical high school student with a fiery passion for math and music(On second thought, add a touch of coding, physics, biology, and chem to that as well)',
   links: [
     /*{
       name: 'Twitter',
